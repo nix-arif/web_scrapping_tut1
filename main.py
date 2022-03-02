a548fbcadf82f5b117e1f6beb39c03a55c38c20b
nix.arif <nix.arif@gmail.com>
2022-03-02 15:19:30 +0800

    df

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import header
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,3 +13,43 @@ url = 'https://www.hubertiming.com/results/2018MLK'
 html = urlopen(url)
 
 soup = BeautifulSoup(html, features="html.parser")
+title = soup.title
+print(title.text)
+
+links = soup.find_all('a', href=True)
+
+# for link in links:
+#   print(link['href'])
+
+data = []
+
+allrows = soup.find_all('tr')
+for row in allrows:
+  row_list = row.find_all('td')
+  dataRow = []
+  for cell in row_list:
+    dataRow.append(cell.text.strip())
+  data.append(dataRow)
+
+# print(data[5:10])
+data = data[5:]
+
+df = pd.DataFrame(data)
+
+
+header_list = []
+col_headers = soup.find_all('th')
+
+for col in col_headers:
+  header_list.append(col.text)
+
+df.columns = header_list
+# print(df.head())
+# print(df.info())
+# print(df.shape)
+
+df2 = df.dropna(axis=0, how='any')
+
+
+df2['ChipTime_minutes'] = pd.to_timedelta(df2['Chip Time'])
+
